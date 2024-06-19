e2fcea791ca138681bfbe0d5d80b718497f22eb1
darleyrodrigues <darleyramos96@gmail.com>
2024-06-18 22:28:07 -0300

    Criação dos componentes

--- food-explorer-frontend/src/main.jsx
+++ food-explorer-frontend/src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { AuthProvider } from './hooks/auth';
+import { FavoritesProvider } from './hooks/favorites';
+import { CartProvider } from './hooks/cart';
+
+import { Routes } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <AuthProvider>
+        <CartProvider>
+        <FavoritesProvider>
+                <Routes />
+        </FavoritesProvider>
+        </CartProvider>
+        </AuthProvider>
+    </React.StrictMode>
 )
